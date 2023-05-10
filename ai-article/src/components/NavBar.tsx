96fae6c4797d77e4b6ae8456a8bc6b3ad175818d
idealy-ma <r.idealy.ma@gmail.com>
2023-05-10 10:55:23 +0300

    Ok

--- ai-article/src/components/NavBar.tsx
+++ ai-article/src/components/NavBar.tsx
@@ -1,12 +1,30 @@
-export default function NavBar() {
-    return (
-        <div className="i-navbar">
-            <h1 className="i-logo">AI-Blog</h1>
+import { useState, useEffect } from "react";
 
+export default function NavBar() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+        const scrollTop = window.pageYOffset;
+        if (scrollTop > 0) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+        window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    return (
+        <div className={`i-navbar ${scrolled ? 'i-navbar-scrolled' : ''}`}>
+            <h1 className="i-logo">AI-Blog</h1>
             <ul className="i-navlink">
-                <li>Home</li>
-                <li></li>
-                <li></li>
+                <li>Blog</li>
+                <li className="btn btn-login">Connect</li>
             </ul>
         </div>
     )
--- ai-article/src/pages/Index.tsx
+++ ai-article/src/pages/Index.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import ArticleCard from "../components/ArticleCard";
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
 
 export default function Index() {
     const [articles,setArticles] = useState<any[]>();
@@ -18,6 +20,8 @@ export default function Index() {
     },[]);
     return(
         <>
+            <NavBar />
+            <Header imageBg=""/>
             {
                 articles?.map((value)=> <ArticleCard key={value.id} article={value} />)
             }  
